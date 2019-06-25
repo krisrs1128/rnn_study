87bc638ab408a97e7e6edca850b1c9118ce4e61e
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-25 11:13:11 -0400

    loaders for the sinusoid data

---
+++ code/sim/loaders.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from torch.utils.data import Dataset
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+
+class WarpedSinusoids(Dataset):
+    """
+    Read in Warped Sinusoids Data
+
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt
+    >>> x = WarpedSinusoids()
+    >>> plt.scatter(np.arange(len(x[0][1])), x[0][1])
+    """
+    def __init__(self, times_path=None, values_path=None):
+        super(WarpedSinusoids).__init__()
+
+        if not times_path:
+            times_path = "../../data/sinusoid/times.csv"
+
+        if not values_path:
+            values_path = "../../data/sinusoid/values.csv"
+
+        self.times = pd.read_csv(times_path).values
+        self.values = pd.read_csv(values_path).values
+
+    def __getitem__(self, ix):
+        return self.times[ix], self.values[ix]
+
+    def __len__(self):
+        return self.times.shape[0]

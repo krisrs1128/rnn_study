843f6003644a7a5aba89916bae93b87e972b52cd
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-26 10:37:01 -0400

    don't worry about this windowing business, I don't think it's necessary

--- code/sim/loaders.py
+++ code/sim/loaders.py
@@ -29,23 +29,10 @@ class WarpedSinusoids(Dataset):
         self.values = pd.read_csv(values_path).values.astype("float32")
         self.values = torch.from_numpy(self.values).unsqueeze(2)
 
-        # define starting points for strided windows
-        self.window_len = 10
-        self.stride = 4
-
-        self.ts_len = self.times.shape[1]
-        start_pos = np.arange(0, self.ts_len, self.stride)
-        self.start_pos = start_pos[start_pos < self.ts_len - self.window_len]
-        self.n_ts = self.times.shape[0]
-
 
     def __getitem__(self, ix):
-        # extract the window
-        ts_ix = ix % self.n_ts
-        start_ix = ix // self.n_ts
-        window_ix = range(self.start_pos[start_ix], self.start_pos[start_ix] + 10)
-        return self.times[ts_ix, window_ix, :], self.values[ts_ix, window_ix, :]
+        return self.times[ix], self.values[ix, :-1, :], self.values[ix, 1:, :]
 
 
     def __len__(self):
-        return self.n_ts * len(self.start_pos)
+        return self.times.shape[0]

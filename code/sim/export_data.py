55e93d5bb3885fd693d38feca869f4370a862429
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-27 09:10:16 -0400

    cell computations, to verify that I'm getting the gatings correctly

--- code/sim/export_data.py
+++ code/sim/export_data.py
@@ -10,6 +10,9 @@ def sigm_factory(W1, W2, bx, bh):
     """
     Common Operation in Gated RNNs
     """
+    bx = bx.unsqueeze(1) # 1D -> column vector
+    bh = bh.unsqueeze(1)
+
     def sigm(x, h):
         Wx = torch.matmul(W1, x)
         Wh = torch.matmul(W2, h)
@@ -17,30 +20,79 @@ def sigm_factory(W1, W2, bx, bh):
     return sigm
 
 
+def n_factory(W1, W2, bx, bh):
+    bx = bx.unsqueeze(1)
+    bh = bh.unsqueeze(1)
+
+    def n(x, h, r):
+        Wx = torch.matmul(W1, x)
+        Wh = torch.matmul(W2, h)
+        return torch.tanh(Wx + bx + r * (Wh + bh))
+    return n
+
+
+def gru_funs(params):
+    r_fun = sigm_factory(params["r"]["Wi"], params["r"]["Wh"], params["r"]["bi"], params["r"]["bh"])
+    z_fun = sigm_factory(params["z"]["Wi"], params["z"]["Wh"], params["z"]["bi"], params["z"]["bh"])
+    n_fun = n_factory(params["n"]["Wi"], params["n"]["Wh"], params["n"]["bi"], params["n"]["bh"])
+    return r_fun, z_fun, n_fun
+
+
+def gru_cell(r_fun, z_fun, n_fun):
+    def f(x, h):
+        r = r_fun(x, h)
+        z = z_fun(x, h)
+        n = n_fun(x, h, r)
+        h = (1 - z) * n + z * zero_k
+        return h, n, z, r
+    return f
+
+
 def layer_params(weights_ih, weights_hh, bias_ih, bias_hh):
-    zix = range(20, 30) # I know, it's hard coded...
+    rix = range(0, 10) # I know, it's hard coded...
+    zix = range(10, 20)
+    nix = range(20, 30)
     params = {
+        "r" :{
+            "Wi": weights_ih[rix, :],
+            "Wh": weights_hh[rix, :],
+            "bi": bias_ih[rix],
+            "bh": bias_hh[rix]
+        },
         "z" :{
             "Wi": weights_ih[zix, :],
             "Wh": weights_hh[zix, :],
             "bi": bias_ih[zix],
             "bh": bias_hh[zix]
+        },
+        "n" :{
+            "Wi": weights_ih[nix, :],
+            "Wh": weights_hh[nix, :],
+            "bi": bias_ih[nix],
+            "bh": bias_hh[nix]
         }
     }
     return params
 
 
 # looking at one of the trained models
-model = torch.load("../data/models/sinusoid1561562171.pt")
+model = torch.load("../../data/models/sinusoid1561562171.pt")
 
 # default extraction
-ws = WarpedSinusoids()
+ws = WarpedSinusoids("../../data/sinusoid/")
 with torch.no_grad():
     h, h_n, y_hat = model(ws.values[:, :-1, :])
-    pd.DataFrame(y_hat.squeeze().numpy()).to_csv("../data/sinusoid/y_hat.csv", index=False)
+    pd.DataFrame(y_hat.squeeze().numpy()).to_csv("../../data/sinusoid/y_hat.csv", index=False)
 
-# extract the z-gating values for some artificial input
+# verify that these functions agree
 gru = model.featurizer
-params = layer_params(gru.weight_ih_l0, gru.weight_hh_l0, gru.bias_ih_l0, gru.bias_hh_l0)["z"]
-z0_fun = sigm_factory(params["Wi"], params["Wh"], params["bi"], params["bh"])
-z0_fun(-torch.ones(1), torch.zeros(10))
+h, hn = gru(torch.zeros((50, 1, 1)))
+h[0]
+
+# computations "by hand" agree
+params = layer_params(gru.weight_ih_l0, gru.weight_hh_l0, gru.bias_ih_l0, gru.bias_hh_l0)
+f0 = gru_cell(*gru_funs(params))
+h01, n01, z01, r01 = f0(torch.zeros((1, 1)), torch.zeros((10, 1)))
+params = layer_params(gru.weight_ih_l1, gru.weight_hh_l1, gru.bias_ih_l1, gru.bias_hh_l1)
+f1 = gru_cell(*(gru_funs(params)))
+h11, n11, z11, r11 = f1(h01, torch.zeros((10, 1)))

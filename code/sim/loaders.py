54dbfa19b00a0b4d9441dae4ab66bc03959811c1
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-27 09:10:06 -0400

    just pass in the datadir

--- code/sim/loaders.py
+++ code/sim/loaders.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+import os.path
 
 
 class WarpedSinusoids(Dataset):
@@ -15,14 +16,14 @@ class WarpedSinusoids(Dataset):
     >>> x = WarpedSinusoids()
     >>> plt.scatter(np.arange(len(x[0][1])), x[0][1])
     """
-    def __init__(self, times_path=None, values_path=None):
+    def __init__(self, data_dir=None):
         super(WarpedSinusoids).__init__()
 
-        if not times_path:
-            times_path = "../data/sinusoid/times.csv"
+        if not data_dir:
+            data_dir = "../data/sinusoid/"
 
-        if not values_path:
-            values_path = "../data/sinusoid/values.csv"
+        times_path = os.path.join(data_dir, "times.csv")
+        values_path = os.path.join(data_dir, "values.csv")
 
         # read saved warped sinusoid data from file
         self.times = pd.read_csv(times_path).values.astype("float32")

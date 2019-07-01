f493a8c0d89c61621565b12676e2bd646b1c2921
Kris Sankaran <sankaran.kris@gmail.com>
2019-07-01 13:09:04 -0400

    draw the warping

--- code/vis/minimal_dynamics.js
+++ code/vis/minimal_dynamics.js
@@ -13,8 +13,9 @@ var svg_elem = d3.select("body")
     .attrs({"width": width,
            "height": height});
 
+var gid = ["x_fun", "x_star_fun", "hx_star_scatter", "hx_scatter", "warping"];
 svg_elem.selectAll("g")
-    .data(["x_fun", "x_star_fun", "hx_star_scatter", "hx_scatter"]).enter()
+    .data(gid).enter()
     .append("g")
     .attr("id", (d) => d);
 
@@ -33,6 +34,12 @@ var scales = {
         "x": d3.scaleLinear()
             .domain([-1, 1])
             .range([200, 0])
+    },
+    "c": {
+        "time": d3.scaleLinear()
+            .range([400, 500]),
+        "time_star": d3.scaleLinear()
+            .range([200, 100])
     }
 };
 
@@ -48,6 +55,7 @@ var d0 = {"time": 0, "x": 0, "h": 0, "h_next": 0},
 var dynamics = evaluate_dynamics(times, d0, x, f_rnn);
 var warped_dynamics = evaluate_dynamics(c_warp(times), d0, x, f_rnn);
 for (var i = 0; i < N; i++) {
+    warped_dynamics[i]["time_star"] = warped_dynamics[i]["time"];
     warped_dynamics[i]["time"] = times[i];
 }
 
@@ -57,3 +65,13 @@ draw_x_fun(svg_elem.select("#x_star_fun"), warped_dynamics, scales.x_fun);
 
 draw_xh_scatter(svg_elem.select("#hx_scatter"), dynamics, scales.hx);
 draw_xh_scatter(svg_elem.select("#hx_star_scatter"), warped_dynamics, scales.hx);
+
+svg_elem.select("#warping")
+    .selectAll("circle")
+    .data(warped_dynamics).enter()
+    .append("circle")
+    .attrs({
+        "cx": (d) => scales.c.time(d["time"]),
+        "cy": (d) => scales.c.time_star(d["time_star"]),
+        "r": 1
+    });

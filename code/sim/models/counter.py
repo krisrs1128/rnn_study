2bf4c0a62f76e9812b3ed4757b22af943b8a29b6
Kris Sankaran <sankaran.kris@gmail.com>
2019-07-20 22:56:00 -0400

    training for counter problem

---
+++ code/sim/models/counter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Classes of RNNs
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CounterModel(nn.Module):
+    """
+    Wrapper for RNN Models
+
+    Just a light wrapper around nn.RNN, nn.GRU, and nn.LSTM. I'm not exactly
+    sure how we're going to extract gating values from these, but we'll get to
+    that later.
+
+    Examples
+    --------
+    >>> x = DataLoader(CounterData())
+    >>> _, y, count = next(iter(x))
+    >>>
+    >>> # to predict the next timepoint from the previous ones
+    >>> h, h_n, y_hat = CounterModel()(y[:, :, :])
+    >>> plt.scatter(y[:, 1:, :].detach().numpy(), y_hat.detach().numpy())
+    """
+    def __init__(self, unit_type="GRU"):
+        super(CounterModel, self).__init__()
+        self.n_layers = 2
+        self.input_size = 1
+        self.output_size = 10
+        self.hidden_size = 10
+        params = {
+            "input_size": self.input_size,
+            "hidden_size": self.hidden_size,
+            "num_layers": self.n_layers
+        }
+
+        self.featurizer = nn.GRU(**params)
+        self.regressor = nn.Linear(self.n_layers * self.hidden_size, self.output_size)
+
+
+    def forward(self, x):
+        h, h_n = self.featurizer(x.transpose(1, 0))
+        h, h_n = h.transpose(1, 0), h_n.transpose(1, 0)
+        return h, h_n, self.regressor(h_n.flatten(1))
---
+++ code/sim/train_counter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Training Sequence Models
+"""
+from loaders import CounterData
+from models.counter import CounterModel
+from torch.utils.data import DataLoader
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def train(model, iterator, optimizer, loss_fun):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    epoch_loss = 0
+    model.train()
+
+    for _, y, count in iterator:
+        optimizer.zero_grad()
+
+        _, _, y_hat = model(y.to(device))
+        loss = loss_fun(y_hat.squeeze(), count.to(device))
+
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    return model, epoch_loss / len(iterator)
+
+
+if __name__ == '__main__':
+    opts = {"train": {"n_epochs": 50, "lr": 1e-3}}
+    model = CounterModel()
+    optimizer = torch.optim.Adam(model.parameters(), lr=opts["train"]["lr"])
+    cd = CounterData()
+
+    for epoch in range(opts["train"]["n_epochs"]):
+        model, train_loss = train(model, DataLoader(cd, batch_size=20), optimizer, nn.CrossEntropyLoss())
+        print("\tTrain Loss: {}".format(train_loss))
+
+    torch.save(model, "sinusoid{}.pt".format(int(time.time())))
